91f012aa1cf89c880b7c03fde18c49e7b7fc8f63
Sergey Novikov <snov@snov.me>
2017-07-08 22:34:49 +0200

    Use environment for bot configuration

--- lex-api.js
+++ lex-api.js
@@ -3,11 +3,14 @@
 const AWS = require('aws-sdk');
 const lexruntime = new AWS.LexRuntime();
 
+const BotName = process.env.BOT_NAME;
+const BotAlias = process.env.BOT_ALIAS;
+
 module.exports.send = function (userId, text) {
     return new Promise((resolve, reject) => {
         let params = {
-            botAlias: 'Dev',
-            botName: 'DictBot',
+            botName: BotName,
+            botAlias: BotAlias,
             contentType: 'text/plain; charset=utf-8',
             inputStream: text,
             userId: userId,
