a233493146b5beec4f74b4d452116a61ff9026a5
Sergey Novikov <snov@snov.me>
2017-07-15 20:11:49 +0200

    Repeat oldest words first

--- db.js
+++ db.js
@@ -87,7 +87,7 @@ module.exports.getRandomTestWord = function (user) {
               INNER JOIN progress p
                 ON w.id = p.word_id
                    AND p.user_id = @userId
-            ORDER BY newid()`,
+            ORDER BY p.next_repeat`,
             {
                 userId: user.id
             }
