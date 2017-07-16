995d0fa16981d3031a3a2c6b0d0d4899b85079f0
Sergey Novikov <snov@snov.me>
2017-07-16 11:47:18 +0200

    Adjust levels

--- db.js
+++ db.js
@@ -139,9 +139,9 @@ function connect() {
 
 function difficultyLevelByUserLevel(level) {
     return [
-        [1, 2, 3],
-        [2, 3, 4, 5],
-        [2, 3, 4, 5, 6]
+        [1, 2],
+        [2, 3, 4],
+        [2, 3, 4, 5]
     ][level];
 }
 
